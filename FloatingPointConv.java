f0773d99c1636bb518000b0d634033ede12b3dc3
“kevin” <kevinle1878@gmail.com>
2025-02-11 19:06:57 -0800

    Skeleton of the program

--- FloatingPointConv.java
+++ FloatingPointConv.java
@@ -1,6 +1,25 @@
+import java.util.*;
+
 public class FloatingPointConv {
     public static void main(String[] args){
-        //something edit
-        System.out.println("Hello word");
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            try {
+                float number = in.nextFloat(); // get input number
+                String standard_rep = Standard_FLoatingPoint(number);
+                String IEEE_rep = IEEE_FloatingPoint(number);
+                
+            } catch (Exception e) {
+                System.out.println("Invalid input, please provide a decimal number...\n-----\n");
+            }
+        }
+    }
+
+    private static String Standard_FloatingPoint(float number){
+        
+    }
+
+    private static String IEEE_FloatingPoint(float number){
+
     }
 }
