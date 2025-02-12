f8ff78a90a672ede792081a7fb213975f2043935
FranklinDo408 <franklindo408@gmail.com>
2025-02-11 23:55:08 -0800

    applied your changes for now

--- FloatingPointConv.java
+++ FloatingPointConv.java
@@ -23,23 +23,41 @@ public class FloatingPointConv {
     }
     private static String DecimalToBinary(float number){
         StringBuilder bin_res = new StringBuilder();
-        float left_radix = (float)Math.floor(number);
+        float left_radix = (float)Math.floor(number); //just the whole numbers
         int whole_number = (int)left_radix;
         while (whole_number > 0){
             bin_res.append((char)whole_number%2);
             whole_number /= 2;
         }
-        System.out.println(bin_res.reverse().toString());
+        bin_res.reverse(); 
+        int exponent = bin_res.length();
+
+        float right_radix = number - left_radix;
+        System.out.println("right radix at the beginning " + right_radix);
+
+        for (int i = 0; i < 8; i++) {//going to keep finding a good solution but honestly, no luck so fair 11:50pm
+            float temp = right_radix * 2;
+            right_radix = (float)Math.floor(temp);
+            bin_res.append((char)(int)(temp - right_radix));
+            right_radix = temp - right_radix;
+        }
+
+        System.out.println(right_radix + "word");
+        System.out.println(bin_res.toString());
+
         return "";
     }
     private static String Simplified_FloatingPoint(float number){
         // The simplified model uses 1 bit for the sign, 5 bits for the exponent, and 8 bits for the significand.  
         DecimalToBinary(number);
+
         return "";
     }
 
     private static String IEEE_FloatingPoint(float number){
         // The IEEE-754 single precision floating point standard uses an 8-bit exponent (with a bias of 127) and a 23-bit significand.  
+        DecimalToBinary(number);
+
         return "";
     }
 }
