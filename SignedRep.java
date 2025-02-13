b57f5d3a181c3da7d61d23fd2df4c3b6c0caee42
“kevin” <kevinle1878@gmail.com>
2025-02-12 20:19:28 -0800

    First commit of part 2

---
+++ SignedRep.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SignedRep {
+
+    public static void main(String[] args){
+
+    }
+
+    public static void InputCaller(){
+        Scanner in = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Input a Binary in Signed Magnitude or 2's Complement");
+            String response = in.nextLine();
+            // TODO: check if response is valid
+
+            System.out.println("Which representation? Signed or 2's Complement");
+            String rep = in.nextLine();
+        }   
+    }
+}
